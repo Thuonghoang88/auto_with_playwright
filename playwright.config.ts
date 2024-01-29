5c8e7fbd56ba7aba22adb9fcf9d83f8a8ff587b4
Thuonghoang88 <36090621+Thuonghoang88@users.noreply.github.com>
2024-01-29 23:43:44 +0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  reporter: 'allure-playwright',
+  reporter: 'html',
   use: {
     trace: 'on-first-retry',
   },
